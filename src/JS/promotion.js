08394f361dd8c0122840d749ce95320c70b3166f
taha <mlaikitaha29@gmail.com>
2024-11-12 11:38:41 +0100

    add some changes

--- src/JS/promotion.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelector(".nav-links")
-document.querySelector(".burger-icon").addEventListener("click" , function () {
-    navLinks.style.left= "0%";
-})
-
-document.querySelector(".close-icon").addEventListener("click", function () {
-    navLinks.style.left= "-100%";
-})
