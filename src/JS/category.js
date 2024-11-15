72181028fbe7de1fb0b6b9d0cd73d96020f7420d
laamiri-kaoutar <kaoutarlaamiri355@gmail.com>
2024-11-15 17:26:06 +0100

    create the function that display cards in list

--- src/JS/category.js
+++ src/JS/category.js
@@ -14,17 +14,25 @@ const createRatingStars = (rating) => {
   return stars;
 }
 
+
+
 const RenderCards = async () => {
+
   const res = await fetch("http://localhost:3000/products");
   data = await res.json();
 
+  categoriesCont.classList.add("grid" , "grid-cols-2" , "mt-8" , "md:grid-cols-3" , "lg:grid-cols-4" , "gap-5" , "md:gap-x-10" , "lg:gap-x-20")
+  display_grid.classList.add("text-gray-400")
+  display_list.classList.remove("text-gray-400")
+
+ console.log("this is working")
   // Set innerHTML to empty string before loop
   categoriesCont.innerHTML = "";
 
   data.map((ele) => {
     categoriesCont.innerHTML += `
       <div
-        class="card background_img w-full  flex-col justify-between rounded-3xl px-3 py-4 text-white h-[330px] lg:h-[400px] overflow-hidden"
+        class="card product w-full  flex-col justify-between rounded-3xl px-3 py-4 text-white h-[330px] lg:h-[400px] overflow-hidden"
       >
         <img
           src="${ele.image || "../Assets/Images/t-shirt-placeholder.png"}"
@@ -50,6 +58,8 @@ const RenderCards = async () => {
       </div>
     `;
   });
+
+  cardsPerPage = 8;
   displayCards();
 
 };
@@ -61,7 +71,7 @@ RenderCards();
 let pagesindex = document.querySelector(".pagination").querySelectorAll("li")
 
 let currentPage = 1;
-let numberPages = 4;
+let numberPages = 3;
 let cardsPerPage = 8;
 
 function activePage() {
@@ -73,8 +83,7 @@ function activePage() {
     event.target.classList.add("selected")
     currentPage= event.target.value;
     console.log(currentPage)
-    displayCards();
-
+    displayCards(); 
 }
 
 function prevPage() {
@@ -116,7 +125,7 @@ function nextPage() {
 }
 
 function displayCards() {
-  let cards = document.querySelectorAll(".card")
+  let cards = document.querySelectorAll(".product")
     let start = (currentPage-1)*cardsPerPage ;
     let end = currentPage*cardsPerPage - 1;
 
@@ -140,4 +149,57 @@ function displayCards() {
 
 //  ==> end of pagination section
 
+// ==> Start the function that display product in list 
+const display_list =document.querySelector(".display_list")
+const display_grid =document.querySelector(".display_grid")
+const toList = async () => {
+   cardsPerPage = 4;
+  const res = await fetch("http://localhost:3000/products");
+  data = await res.json();
+
+  categoriesCont.classList.remove("grid" , "grid-cols-2" , "mt-8" , "md:grid-cols-3" , "lg:grid-cols-4" , "gap-5" , "md:gap-x-10" , "lg:gap-x-20")
+  display_grid.classList.remove("text-gray-400")
+  display_list.classList.add("text-gray-400")
+
+
+  // Set innerHTML to empty string before loop
+  categoriesCont.innerHTML = "";
+
+  data.map((ele) => {
+    categoriesCont.innerHTML += `
+          <div class="product justify-between rounded-md my-6 bg-gray-200 p-4 max-w-[1000px] ">
+          <div class="card rounded-md flex justify-center items-center w-[35%] lg: ">
+            <img class="w-2/5 h-auto " src="${ele.image || "../Assets/Images/parfume.png"}" alt=" product image">
+          </div>
+          <div class="flex flex-col justify-center w-[60%]  gap-1 sm:gap-3  ">
+            <h2 class="font-bold  sm:text-2xl md:text-3xl  ">${ele.title}</h2>
+            <h3 class="font-semibold sm:text-xl md:text-2xl  ">${ele.price}$</h3>
+            <div class="flex w-fit">
+              ${createRatingStars(ele.stars)}  
+            </div>
+            <p class="text-sm md:text-lg   sm:text-base ">${ele.description}</p>
+            <button class="flex mt-3 px-3 py-[3px] gap-[4px] items-center self-end  rounded-md text-black font-semibold  bg-white sm:text-lg ">
+              <span class=" text-sm sm:text-lg sm:font-bold"> Add To Cart </span>
+              <i class="fa-solid fa-cart-shopping"></i> 
+            </button>
+          </div>
+
+        </div>
+
+    `;
+  });
+  displayCards();
+
+};
+// ==> End the function that display product in list 
+
+// here goes the event listner to switche from list to grid 
+// const display_list =document.querySelector(".display_list")
+// const display_grid =document.querySelector(".display_grid")
+
+// display_grid.addEventListener("click",RenderCards());
+// display_list.addEventListener("click",toList())
+
+// end of event listner to switche from list to grid 
+ 
 
