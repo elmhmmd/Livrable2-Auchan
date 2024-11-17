b9a6a552e825499d8c52c2bc0ef10d4f9925d3a7
Radioserk <boujahdachraf@gmail.com>
2024-11-17 20:28:43 +0100

    Js vider le formulaire

--- src/JS/contact.js
+++ src/JS/contact.js
@@ -1,20 +1,13 @@
-function toggleMenu() {
-    const menu = document.getElementById("mobileMenu");
-    menu.classList.toggle("hidden");
-  }
-
-//   form fuction
-
+// Fonction pour vider le formulaire
 function clearForm() {
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("message").value = "";
-  }
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("message").value = "";
+}
 
-  // Submit form event listener
-  document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Your message has been sent!");
-    clearForm();
-  });
+// Fonction pour valider le formulaire
+function validateForm() {
+  let isValid = true; // Initialiser à vrai
+
+  
